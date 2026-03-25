84d100bc20886ef339e7601e81e52bd7bc456353
rodbren <rodbren@users.noreply.github.com>
2026-03-25 13:01:28 -0700

    Pin AWS provider < 5.56.0 to avoid ValidateStateMachineDefinition permission issue

--- terraform/states.tf
+++ terraform/states.tf
@@ -5,6 +5,4 @@ resource "aws_sfn_state_machine" "aft_account_provisioning_customizations" {
   name       = "aft-account-provisioning-customizations"
   role_arn   = aws_iam_role.aft_states.arn
   definition = templatefile("${path.module}/states/customizations.asl.json", {})
-
-  validation_level = "DISABLED"
 }
